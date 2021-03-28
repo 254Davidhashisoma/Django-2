7d32422ba561664222972207a0e5c646aff4540b
254Davidhashisoma <davidhashisoma95@gmail.com>
2021-03-28 15:39:22 +0300

    passing data moddels

--- crm1/accounts/forms.py
+++ crm1/accounts/forms.py
@@ -6,4 +6,12 @@ from django import forms
 
 from .models import order
 
+class OrderForm(ModelForm):
+    class Meta:
+        model = order
+        fields = '__all__'
 
+class createUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']        
