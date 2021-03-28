2d342c263f657de30588f8ed4ed6afd3aedab125
254Davidhashisoma <davidhashisoma95@gmail.com>
2021-03-28 17:20:43 +0300

    templates inheritance

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -36,9 +36,17 @@ class Order(models.Model):
     STATUS = (
         ('Pending', 'Pending'),
         ('Out for delivery', 'Out for delivery'),
-        
-    )                 
+        ('Delivered', 'Delivered'),
+        )                 
 
+    Customer = models.ForeignKey(Customer,null=True, on_delete=models.SET_NULL)
+    Product =models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+    note = models.CharField(max_length=1000, null=True)
 
+    def __str__(self):
+        return self.name
+    
             
     
