3b8fd9a4006a8d250664a62564d97b9bc2230a96
254Davidhashisoma <davidhashisoma95@gmail.com>
2021-03-29 18:50:53 +0300

    adding config delopying

--- crm1/crm1/settings.py
+++ crm1/crm1/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,3 +134,7 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
+django_heroku.settings(locals())
